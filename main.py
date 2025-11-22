97e5f6ae0d467775821e0cc74d36fcba815b87c4
Varga Norbert <vnorbi02@gmail.com>
2025-11-22 13:36:48 +0100

    init

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, File, UploadFile, Form
+from azure.storage.blob import BlobServiceClient
+import requests
+import os
+import time
+
+app = FastAPI()
+
+@app.post("/translate")
+async def translate_file(
+    file: UploadFile = File(...),
+    lang: str = Form(...)
+):
+    # Read text from uploaded file
+    content = (await file.read()).decode("utf-8")
+
+    # Translator API
+    translator_key = os.environ["TRANSLATOR_KEY"]
+    endpoint = os.environ["TRANSLATOR_ENDPOINT"]
+    region = os.environ["TRANSLATOR_REGION"]
+
+    url = f"{endpoint}/translate?api-version=3.0&to={lang}"
+
+    response = requests.post(
+        url,
+        json=[{"Text": content}],
+        headers={
+            "Ocp-Apim-Subscription-Key": translator_key,
+            "Ocp-Apim-Subscription-Region": region,
+        }
+    )
+
+    translated = response.json()[0]["translations"][0]["text"]
+
+    # Upload to Blob
+    blob_conn = os.environ["BLOB_CONNECTION_STRING"]
+    blob_service = BlobServiceClient.from_connection_string(blob_conn)
+    container = blob_service.get_container_client("output-files")
+
+    file_name = f"translated-{int(time.time())}.txt"
+    container.upload_blob(file_name, translated, overwrite=True)
+
+    return {"translated": translated, "blob": file_name}
